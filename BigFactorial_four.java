db717cba4bc5e2007437b8783157a35d87c283bf
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-04-19 20:45:57 +0600

    Create BigFactorial_four.java
    Big factorials in toph.co

---
+++ BigFactorial_four.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		int n = 1;
+		for(int i=1; i<=x; i++){
+			n*=i;
+			if(n>9999){
+				n = n%10000;
+			}
+		}
+		System.out.println(n);
+	}
+}
