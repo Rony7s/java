fd2c9c420050b4876663b5e14816f23f00237e0a
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-05-25 19:23:14 +0600

    Add files via upload
    read data form database

---
+++ database/LearnMySql01.java
@@ -0,0 +1,27 @@
+package learnmysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LearnMySql01S{
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/jdbcdemo", "root", ""
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from student");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+                ;
+
+            }
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
